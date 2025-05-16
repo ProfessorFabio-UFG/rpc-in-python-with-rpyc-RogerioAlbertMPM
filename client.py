6e0ab99f5daf8a68ae4854521dbd9e50df861d42
RogerioAlbertMPM <rogerioalbertmozer@hotmail.com>
2025-05-16 15:46:09 -0300

    answer

--- client.py
+++ client.py
@@ -7,3 +7,10 @@ class Client:
   conn.root.exposed_append(5)       # Call an exposed operation,
   conn.root.exposed_append(6)       # and append two elements
   print (conn.root.exposed_value())   # Print the result
+
+  txt = "Python RPC DistSys"
+  print("uppercase: ", conn.root.exposed_uppercase(txt))
+  print("lowercase: ", conn.root.exposed_lowercase(txt))
+  print("reverse: ", conn.root.exposed_reverse(txt))
+
+  conn.close()
--- server.py
+++ server.py
@@ -11,6 +11,15 @@ class DBList(rpyc.Service):
 
   def exposed_value(self):
     return self.value
+  
+  def exposed_uppercase(self, text):
+    return text.upper()
+  
+  def exposed_lowercase(self, text):
+    return text.lower()
+  
+  def exposed_reverse(self, text):
+    return text[::-1]
 
 if __name__ == "__main__":
   server = ThreadedServer(DBList(), port = PORT)
